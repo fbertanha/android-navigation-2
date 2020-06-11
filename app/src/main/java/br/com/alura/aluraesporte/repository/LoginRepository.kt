b07d05b80ab04215651c933229e83aa5e46b7cd7
felipe.bertanha <felipe.bertanha@capitaletecnologia.com.br>
2020-06-10 23:26:59 -0300

    aula 1.3 - added sharePrefs to save login status

---
+++ app/src/main/java/br/com/alura/aluraesporte/repository/LoginRepository.kt
@@ -0,0 +1,19 @@
+package br.com.alura.aluraesporte.repository
+
+import android.content.SharedPreferences
+import androidx.core.content.edit
+
+
+private const val KEY_LOGADO = "key_logado"
+
+class LoginRepository(private val preferences: SharedPreferences) {
+
+    fun loga() {
+        preferences.edit {
+            putBoolean(KEY_LOGADO, true)
+        }
+    }
+
+    fun estaLogado(): Boolean = preferences.getBoolean(KEY_LOGADO, false)
+
+}
--- app/src/main/java/br/com/alura/aluraesporte/ui/fragment/LoginFragment.kt
+++ app/src/main/java/br/com/alura/aluraesporte/ui/fragment/LoginFragment.kt
@@ -7,7 +7,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import br.com.alura.aluraesporte.R
+import br.com.alura.aluraesporte.ui.viewmodel.LoginViewModel
 import kotlinx.android.synthetic.main.login.*
+import org.koin.android.viewmodel.ext.android.viewModel
 
 /**
  * Created by felipebertanha on 08/June/2020
@@ -18,6 +20,8 @@ class LoginFragment : Fragment() {
     private val navigator by lazy {
         findNavController()
     }
+
+    private val viewModel by viewModel<LoginViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,8 +34,17 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        login_botao_logar.setOnClickListener {
-            navigator.navigate(LoginFragmentDirections.actionLoginToListaProdutos())
+        if (viewModel.estaLogado()) {
+            vaiParaListaProdutos()
         }
+
+        login_botao_logar.setOnClickListener {
+            viewModel.loga()
+            vaiParaListaProdutos()
+        }
+    }
+
+    private fun vaiParaListaProdutos() {
+        navigator.navigate(LoginFragmentDirections.actionLoginToListaProdutos())
     }
 }
